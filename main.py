f13058494c85e4b6f5fb7e5a7044aec65a57789a
ynejmidev <ynejmidev@noreply.com>
2024-11-24 21:00:42 +0100

    update main.py to oauth2 example

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
