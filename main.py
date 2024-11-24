ca019dbce4b5a68842816011fbeed77cac1c9027
ynejmidev <ynejmidev@noreply.com>
2024-11-24 21:48:26 +0100

    add /token path (simple auth logic)

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI
 
-from utils import get_current_user, User
+from fastapi.security import OAuth2PasswordRequestForm
+from utils import *
 
 app = FastAPI()
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
 
 
 @app.get("/users/me")
