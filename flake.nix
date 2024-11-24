79596574fd4953b0e98e76a778f347925bc763eb
ynejmidev <ynejmidev@noreply.com>
2024-11-24 20:40:58 +0100

    env init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    # this is equivalent to `nixpkgs = { url = "..."; };`
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  # now a set 👇
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              uv
+            ];
+          };
+      }
+    );
+}
