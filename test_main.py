db1239268e944dcbfcf32db22dc271e894dc2585
ynejmidev <ynejmidev@noreply.com>
2024-11-25 02:03:54 +0100

    group test in one func

--- test_main.py
+++ test_main.py
@@ -1,15 +1,31 @@
-import pytest
 import requests
 
+from users import fake_users_db
 
-def test_token():
+
+def test_main():
     data = {
         "grant_type": "password",
         "username": "johndoe",
         "password": "secret",
     }
-    x = requests.post("http://localhost:8000/token", data)
-    res = x.json()
-    assert x.status_code == 200
-    assert res["token_type"].lower() == "bearer"
+    tok = requests.post("http://localhost:8000/token", data)
+    res = tok.json()
 
+    print(res["access_token"])
+
+    assert tok.status_code == 200
+    assert res["token_type"].lower() == "bearer"
+    assert len(res["access_token"]) == 127
+
+    me = requests.get(
+        "http://localhost:8000/users/me",
+        headers={
+            "Authorization": "Bearer " + res["access_token"]
+        },
+    )
+
+    [expected] = fake_users_db.values()
+
+    assert me.status_code == 200
+    assert me.json() == expected
