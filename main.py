679f679b05e315963304b735ea4d4a728ae5bf6b
ynejmidev <ynejmidev@noreply.com>
2024-11-26 17:01:31 +0100

    tmp: better error details

--- main.py
+++ main.py
@@ -11,16 +11,12 @@ from pydantic import ValidationError
 
 @app.exception_handler(ValidationError)
 async def validation_exception_handler(_, exc: ValidationError):
-    err = exc.errors().pop()
-    err["input"] = str(err["input"])  # dict "serialization"
-    return JSONResponse(err, status_code=400)
+    return JSONResponse({"detail": exc.errors().pop()["msg"]}, status_code=400)
 
 
 @app.exception_handler(RequestValidationError)
 async def request_validation_exception_handler(_, exc: RequestValidationError):
-    err = exc.errors()[0]
-    err["input"] = str(err["input"])  # dict "serialization"
-    return JSONResponse(err, status_code=400)
+    return JSONResponse({"detail": exc.errors()}, status_code=400)
 
 
 app.add_exception_handler
