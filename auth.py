4979093e4c3c1c1d7d236e94103347046cc0c96b
ynejmidev <ynejmidev@noreply.com>
2024-11-26 16:52:44 +0100

    reorganize auth

--- auth.py
+++ auth.py
@@ -1,95 +1,6 @@
-from datetime import datetime, timedelta, timezone
-from fastapi import APIRouter, Depends
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
-from pydantic import BaseModel
-from sqlmodel import Session, SQLModel, create_engine
-from typing import Annotated
-import jwt
-
-from utils import get_user
-
-SECRET_KEY = "3402751aab4a5d55d02e6e812db0d32d4b3942dae74f6648fe7b630e3df043ec"
-
-ALGORITHM = "HS256"
-
-
-sqlite_url = "postgresql+psycopg2://postgres:postgres@localhost:5432"
-
-engine = create_engine(sqlite_url)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-
-SessionDep = Annotated[Session, Depends(get_session)]
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str
-
-
-def get_token_data(token, credentials_exception):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        # if username is None:
-        #     raise credentials_exception
-        return TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def create_access_token(data, expires_delta: timedelta | None = None):
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-
-    to_encode = {"exp": expire, "sub": data}
-
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-def authenticate_user(session: SessionDep, username: str, password: str):
-    print(username, password)
-    user = get_user(session, username)
-    print(user)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordRequestForm
+from utils import *
 router = APIRouter()
 
 
@@ -103,7 +14,6 @@ async def login_for_access_token(
     session: SessionDep,
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    print(form_data)
     user = authenticate_user(session, form_data.username, form_data.password)
     if not user:
         raise HTTPException(status_code=401, detail="Incorrect username or password")
--- utils.py
+++ utils.py
@@ -1,9 +1,102 @@
+from sqlmodel import select
+
+from typing import Annotated
+
+from fastapi import HTTPException
+
+from datetime import datetime, timedelta, timezone
+from fastapi import Depends
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel, ValidationError
+from sqlmodel import Session, SQLModel, create_engine
+from typing import Annotated
+import jwt
 from sqlmodel import Field, SQLModel, select
+
+
+sqlite_url = "postgresql+psycopg2://postgres:postgres@localhost:5432"
+
+engine = create_engine(sqlite_url)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+TokenDep = Annotated[str, Depends(oauth2_scheme)]
+
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str
+
+
+def get_token_data(token, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        # if username is None:
+        #     raise credentials_exception
+        return TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+
+def create_access_token(data, expires_delta: timedelta | None = None):
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode = {"exp": expire, "sub": data}
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(session: SessionDep, username: str, password: str):
+    user = get_user(session, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 class UserBase(SQLModel):
     username: str = Field(unique=True, index=True, max_length=32)
-    email: str
+    email: str = Field(unique=True)
     is_admin: bool = Field(default=False)
 
 
@@ -11,12 +104,10 @@ class User(UserBase, table=True):
     id: int = Field(primary_key=True, default=None)
     username: str
     hashed_password: str
-    is_admin: bool = Field(default=False)
     is_active: bool = Field(default=True)
 
 
 class UserPublic(UserBase):
-    id: int
     username: str
 
 
@@ -27,9 +118,58 @@ class UserCreate(UserBase):
 class UserUpdate(UserBase):
     email: str
     username: str
+    password: str
+
+
+# class UserInDB(User):
 
 
 def get_user(session, username):
     statement = select(User).where(User.username == username)
     session_user = session.exec(statement).first()
     return session_user
+
+
+def get_current_user(session: SessionDep, token: TokenDep) -> User:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        token_data = TokenData(**payload)
+    except (InvalidTokenError, ValidationError):
+        raise HTTPException(
+            status_code=403,
+            detail="Could not validate credentials",
+        )
+    user = session.get(User, token_data.username)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    if not user.is_active:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return user
+
+
+CurrentUser = Annotated[User, Depends(get_current_user)]
+
+SECRET_KEY = "3402751aab4a5d55d02e6e812db0d32d4b3942dae74f6648fe7b630e3df043ec"
+
+ALGORITHM = "HS256"
+
+
+# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     token_data = get_token_data(token, credentials_exception)
+#     user = get_user(session, username=token_data.username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
+#
+#
+# async def get_current_active_user(
+#     current_user: Annotated[User, Depends(get_current_user)],
+# ):
+#     if current_user.disabled:
+#         raise HTTPException(status_code=400, detail="Inactive user")
+#     return current_user
