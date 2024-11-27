2c2efb6456cbbcfacba1e6f0bbacca4a130933b4
ynejmidev <ynejmidev@noreply.com>
2024-11-27 17:56:14 +0100

    refactor exceptions

--- utils.py
+++ utils.py
@@ -152,17 +152,15 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         token_data = TokenData(id=payload["sub"])
+        user = session.get(User, token_data.id)
+        if not user:
+            raise InvalidTokenError()
+        return user
     except (InvalidTokenError, ValidationError):
         raise HTTPException(
             status_code=403,
             detail="Could not validate credentials",
         )
-    user = session.get(User, token_data.id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    if not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return user
 
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
