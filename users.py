d34b86c3dbe4114832a81b4bcdbe9f8d43068d86
ynejmidev <ynejmidev@noreply.com>
2024-11-27 17:53:21 +0100

    add permission checks (logged in/admin)

--- users.py
+++ users.py
@@ -7,10 +7,13 @@ router = APIRouter()
 
 @router.get("/", response_model=list[UserPublic])
 def read_users(
+    current_user: CurrentUser,
     session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100,
 ):
+    if not current_user.is_admin:
+        raise HTTPException(status_code=401, detail="You're not an admin")
     users = session.exec(select(User).offset(offset).limit(limit)).all()
     return users
 
@@ -31,12 +34,14 @@ def create_user(user_create: UserCreate, session: SessionDep):
     session.refresh(db_user)
     return db_user
 
+
 @router.get("/me", response_model=UserPublic)
 def read_user_me(current_user: CurrentUser):
     """
     Get current user.
     """
     return current_user
+
 
 @router.get("/{user_id}", response_model=UserPublic)
 def read_user(user_id: int, session: SessionDep):
@@ -47,23 +52,37 @@ def read_user(user_id: int, session: SessionDep):
 
 
 @router.delete("/{user_id}")
-def delete_user(user_id: int, session: SessionDep):
+def delete_user(user_id: int, current_user: CurrentUser, session: SessionDep):
+    if current_user.id != user_id and not current_user.is_admin:
+        raise HTTPException(
+            status_code=401, detail="You are not this user and you are not an admin"
+        )
+
     user = session.get(User, user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail="User not found")
-    session.delete(user)
-    session.commit()
+    if user:
+        session.delete(user)
+        session.commit()
     return {"ok": True}
 
 
 @router.patch("/{user_id}", response_model=UserPublic)
-def update_user(user_id: int, user: UserUpdate, session: SessionDep):
-    user_db = session.get(User, user_id)
-    if not user_db:
+def update_user(
+    current_user: CurrentUser,
+    user_id: int,
+    user_update: UserUpdate,
+    session: SessionDep,
+):
+    if current_user.id != user_id and not current_user.is_admin:
+        raise HTTPException(
+            status_code=401, detail="You are not this user and you are not an admin"
+        )
+
+    user = session.get(User, user_id)
+    if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    user_data = user.model_dump(exclude_unset=True)
-    user_db.sqlmodel_update(user_data)
-    session.add(user_db)
+    user_data = user_update.model_dump(exclude_unset=True)
+    user.sqlmodel_update(user_data)
+    session.add(user)
     session.commit()
-    session.refresh(user_db)
-    return user_db
+    session.refresh(user)
+    return user
